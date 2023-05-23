cf8d871190dcce8a5fc334659d251f6468ace8e5
Rohit <rohitsahu1923@gmail.com>
2023-05-23 17:11:27 +0530

    Initial Commit

--- lib/Strings.dart
+++ lib/Strings.dart
@@ -1,5 +1,6 @@
+// ignore: file_names
 class LogConstants {
-  static const String logName = "IPPRO";
+  static const String logName = "Class";
   static const String className = "Class Name:";
   static const String methodName = "Method Name:";
   static const String apiUrl = "Method Name:";
--- lib/save_logger.dart
+++ lib/save_logger.dart
@@ -1,6 +1,5 @@
 import 'dart:io';
 import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
 import 'package:open_file/open_file.dart';
 import 'package:path_provider/path_provider.dart';
 import 'Strings.dart';
